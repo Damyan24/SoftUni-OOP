385e747d0515a96bebc438666a42d84a22cf19c9
Damyan <bpower294@gmail.com>
2023-07-11 11:09:11 +0300

    Enums

---
+++ src/CardSuit/Main.java
@@ -0,0 +1,11 @@
+package CardSuit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Card Suits:");
+        for(Suits allSuits : Suits.values()){
+            System.out.println(allSuits.toString());
+        }
+    }
+}
---
+++ src/TrafficLights/Traffic.java
@@ -0,0 +1,36 @@
+package TrafficLights;
+
+import java.util.Arrays;
+
+public class Traffic {
+
+    private String[] traffic;
+
+    public Traffic(String[] traffic) {
+        this.traffic = traffic;
+    }
+
+    public void changeColor() {
+
+
+        for (int i = 0; i < traffic.length; i++) {
+            if (traffic[i].equals("RED")) {
+                traffic[i] = "GREEN";
+            } else if (traffic[i].equals("GREEN")) {
+                traffic[i] = "YELLOW";
+            } else if (traffic[i].equals("YELLOW")) {
+                traffic[i] = "RED";
+            }
+        }
+
+
+    }
+
+    public String toAWS() {
+        String line = "";
+       for(String s : traffic){
+           line = line + s + " ";
+       }
+       return line;
+    }
+}
